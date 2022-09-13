b3e07cc9b3d9bba68f213a956b8e65c068bd6c66
Gupuroom <cjsrnr1114@gmail.com>
2022-09-13 10:54:13 +0900

    Initial commit

---
+++ src/test/java/com/delgo/reward/delgorewardserver/DelgoRewardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delgo.reward.delgorewardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelgoRewardServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
